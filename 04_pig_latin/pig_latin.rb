eef145149890fb39736704cb79f18e54da7b0b09
jmy19 <jasonmyers@rogers.com>
2015-02-02 15:22:56 -0500

    added multiple word, multple consonant, and qu exception

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -7,18 +7,29 @@ def consonant?(letter)
 	!vowel?(letter)
 end
 
+def startswith?(string, prefix)
+	string[0..prefix.length-1] == prefix
+end
 
-def translate(string)
+def translateword(string)
 	case
 	when vowel?(string[0])
 		string += "ay"
-	when consonant?(string[0]) && consonant?(string[1])
-		string = string[2..-1] + string[0..1] + "ay"	
+	when consonant?(string[0]) && consonant?(string[1]) && consonant?(string[2])
+		string = string[3..-1] + string[0..2] + "ay"
+	when startswith?(string, "qu"), consonant?(string[0]) && consonant?(string[1])
+		string = string[2..-1] + string[0..1] + "ay"
 	when consonant?(string[0])
 		string = string[1..-1] + string[0] + "ay"
 	end
-
-	
 end
 
-puts translate("ituff")
+def translate(string)
+
+	new_array = string.split(' ').map do |word|
+		translateword(word)
+	end
+	new_array.join(" ")
+end
+
+
--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -23,24 +23,24 @@ describe "#translate" do
     expect(s).to eq("errychay")
   end
 
-  # it "translates two words" do
-  #   s = translate("eat pie")
-  #   expect(s).to eq("eatay iepay")
-  # end
+  it "translates two words" do
+    s = translate("eat pie")
+    expect(s).to eq("eatay iepay")
+  end
 
-  # it "translates a word beginning with three consonants" do
-  #   expect(translate("three")).to eq("eethray")
-  # end
+  it "translates a word beginning with three consonants" do
+    expect(translate("three")).to eq("eethray")
+  end
 
-  # it "counts 'sch' as a single phoneme" do
-  #   s = translate("school")
-  #   expect(s).to eq("oolschay")
-  # end
+  it "counts 'sch' as a single phoneme" do
+    s = translate("school")
+    expect(s).to eq("oolschay")
+  end
 
-  # it "counts 'qu' as a single phoneme" do
-  #   s = translate("quiet")
-  #   expect(s).to eq("ietquay")
-  # end
+  it "counts 'qu' as a single phoneme" do
+    s = translate("quiet")
+    expect(s).to eq("ietquay")
+  end
 
   # it "counts 'qu' as a consonant even when it's preceded by a consonant" do
   #   s = translate("square")
